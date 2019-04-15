cca44c0d331df620f47d3bdfbf09d780c4eddbd7
Victor Manuel <victor@MacBook-Pro-de-victor.local>
2019-04-15 21:59:12 +0100

    added variables and strings

--- hello.py
+++ hello.py
@@ -1,2 +1,21 @@
 msg = "Hello World!"
 print(msg)
+print("---------")
+
+#Variables
+print("Variables")
+number1 = 10
+number2 = 30
+result = number1 + number2
+print(result)
+print("{} + {} = {}".format(number1, number2, result))
+print("---------")
+
+#Strings
+print("Strings")
+greeting = "Hello"
+print(len(greeting))
+print(greeting[2])
+greetingInPart = greeting[0:3]
+print(greetingInPart)
+print("---------")
